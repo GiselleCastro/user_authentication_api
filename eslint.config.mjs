8970551525fc7a9d0e2dc3e686bcbd3b43d44ed1
GiselleCastro <giselle@fisica.ufc.br>
2025-02-15 17:36:39 -0300

    chore: update dependencies and adjust ESLint/Prettier configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['node_modules/**', 'build/**', 'coverage/**', 'logs/**'] },
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
-      "comma-dangle": ["error", "never"],
-      "no-console": "off",
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'never'],
+      'no-console': 'off',
+      'max-len': ['error', { code: 90 }],
     },
   },
   eslintConfigPrettier,
